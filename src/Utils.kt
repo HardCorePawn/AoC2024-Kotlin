0a627785f0c34048ff65d8d614d61705117950a5
HardCorePawn <hardcorepawn69@gmail.com>
2024-12-22 20:51:07 +1300

    Updating Point class with some extra functionality and some math operator functions

--- src/Utils.kt
+++ src/Utils.kt
@@ -17,7 +17,13 @@ enum class Dir(val pair: Pair<Int, Int>) {
 /**
  * Class to represent a (x,y) Point in a 2D grid (because Pair<Int, Int> gets messy :P)
  */
-data class Point(val x: Int, val y: Int)
+data class Point(val x: Int, val y: Int) {
+    operator fun plus(second: Point) = Point(x + second.x, y + second.y)
+    operator fun minus(second: Point) = Point(x - second.x, y - second.y)
+    operator fun times(multiplier: Int) = Point(x * multiplier, y * multiplier)
+
+    fun manhattanDistTo(dest: Point) = abs(x - dest.x) + abs(y - dest.y)
+}
 
 /**
  * Reads lines from the given input txt file.
@@ -53,3 +59,85 @@ fun getManhattenDist(point1: Point, point2: Point): Int {
     return abs(point1.x - point2.x) + abs(point1.y - point2.y)
 }
 
+/**
+ * Represents a graph node
+ *
+ * @property value The value of the node.
+ * @property neighbours The list of neighbouring nodes.
+ */
+data class Node<T>(val value: T, val neighbours: MutableList<Node<T>> = mutableListOf())
+
+/**
+ * Represents a graph
+ *
+ * @property nodes The list of nodes in the graph
+ */
+class Graph<T> {
+    val nodes: MutableList<Node<T>> = mutableListOf()
+
+    /**
+     * Adds a node to the graph.
+     *
+     * @param value The value of the node.
+     * @return The newly added node.
+     */
+    fun addNode(value: T): Node<T> {
+        val node = Node(value)
+        nodes.add(node)
+        return node
+    }
+
+    /**
+     * Adds an edge between two nodes in the graph.
+     *
+     * @param node1 The first node.
+     * @param node2 The second node
+     */
+    fun addEgde(node1: Node<T>, node2: Node<T>) {
+        node1.neighbours.add(node2)
+        node2.neighbours.add(node1)
+    }
+
+    /**
+     * Finds the shortest path between two nodes in the graph using breadth-first search (BFS)
+     *
+     * @param start The starting node.
+     * @param end The ending node.
+     * @return The shortest path as a list of nodes, or an empty list if no path exists
+     */
+    fun findShortestPath(start: Node<T>, end: Node<T>): List<Node<T>> {
+        val queue = ArrayDeque<Node<T>>()
+        val visited = mutableSetOf<Node<T>>()
+        val parentMap = mutableMapOf<Node<T>, Node<T>?>()
+
+        queue.add(start)
+        visited.add(start)
+        parentMap[start] = null
+
+        while (queue.isNotEmpty()) {
+            val current = queue.removeFirst()
+
+            if (current == end) {
+                // Reconstruct the path from end to start using the parent map
+                val path = mutableListOf<Node<T>>()
+                var node: Node<T>? = current
+                while (node != null) {
+                    path.add(node)
+                    node = parentMap[node]
+                }
+                return path.reversed()
+            }
+
+            for (neighbour in current.neighbours) {
+                if (neighbour !in visited) {
+                    queue.add(neighbour)
+                    visited.add(neighbour)
+                    parentMap[neighbour] = current
+                }
+            }
+        }
+
+        // no path found :(
+        return emptyList()
+    }
+}
